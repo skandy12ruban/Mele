6d15176a04173a82682b701d57e72cc04d112bf1
skandy12ruban <skandyruban@gmail.com>
2018-10-05 12:40:29 +0530

    added demo api

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -8,16 +8,17 @@ import 'rxjs/add/observable/interval'
 
 
 @Component({
-  
+
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  _songs :object[];
+  _songs: object[];
   songDisplay: object[];
   choice: string;
   currentSong: object;
+  list: object[];
   isSongPlaying: boolean;
   songRange: number = 0;
   timerVar;
@@ -27,58 +28,61 @@ export class AboutPage {
   constructor(public navCtrl: NavController, private http: Http) {
   }
 
-  ngOnInit(){
-    this.http.get("../assets/song.json")
-    .map((Response)=>Response.json())
-    .subscribe(
-      (data)=> this.songs = data['songs']
-    );
-    }
+  ngOnInit() {
+    this.http.get("https://jsonplaceholder.typicode.com/todos")
+      .map((Response) => Response.json())
+      .subscribe(
+        (data) => {
+          this.list = data;
+          console.log(this.list)
+        }
+      );
+  }
 
-    set songs(songs: any){
-      this._songs = songs;
-      this.songDisplay = songs;
+  set songs(songs: any) {
+    this._songs = songs;
+    this.songDisplay = songs;
+  }
+  get songs() {
+    return this._songs;
+  }
+  showSongs(event) {
+    if (this.choice != '') {
+      this.songDisplay = this.songs.filter(song => song['artist'].includes(this.choice));
+    } else {
+      this.songDisplay = this.songs;
     }
-    get songs(){
-      return this._songs;
-    }
-    showSongs(event){
-      if(this.choice != ''){
-        this.songDisplay = this.songs.filter(song=>song['artist'].includes(this.choice));      
-      }else{
-        this.songDisplay = this.songs;
-      }
-    }
+  }
 
-    playSong(choice){
-      this.currentSong = choice;
-      this.isSongPlaying = true;
-      this.startTimer(this._duration);
-    }
+  playSong(choice) {
+    this.currentSong = choice;
+    this.isSongPlaying = true;
+    this.startTimer(this._duration);
+  }
 
-    TooglePlay(){
-      this.isSongPlaying = !this.isSongPlaying;
-      if(this.isSongPlaying){
-        this.startTimer(this.timer);
-      }else{
-        this.timerVar.unsubscribe();
-      }
+  TooglePlay() {
+    this.isSongPlaying = !this.isSongPlaying;
+    if (this.isSongPlaying) {
+      this.startTimer(this.timer);
+    } else {
+      this.timerVar.unsubscribe();
     }
+  }
 
-    changeRange(event){
-      console.log(this.songRange);
-    }
+  changeRange(event) {
+    console.log(this.songRange);
+  }
 
-    startTimer(duration) {
-      this.timerVar = Observable.interval(1000) // milliseconds
-        .subscribe(x => {
-          this.timer = duration - x;
-          if (this.timer < 0) {
-            this.timerVar.unsubscribe();
-          } else {
-            console.log(this.timer);
-          }
-        })
+  startTimer(duration) {
+    this.timerVar = Observable.interval(1000) // milliseconds
+      .subscribe(x => {
+        this.timer = duration - x;
+        if (this.timer < 0) {
+          this.timerVar.unsubscribe();
+        } else {
+          console.log(this.timer);
+        }
+      })
   }
 
 }
